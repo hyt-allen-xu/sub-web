4695b3de876823fa0adea349afa50a9a9da4037e
Allen <73211772+hyt-allen-xu@users.noreply.github.com>
2021-02-24 15:38:04 +0800

    Update Subconverter-local.vue

--- src/views/Subconverter-local.vue
+++ src/views/Subconverter-local.vue
@@ -265,29 +265,19 @@ export default {
         },
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
-          "sub-beta.now.sh (自动编译最新版本后端-测试）": "https://sub-beta.now.sh/sub?",
-          "subcon.dlj.tf(subconverter作者提供)":
+          "subconverter-mocha.vercel.app (自动编译最新版本后端-测试）": "https://subconverter-mocha.vercel.app/sub?",
+          "subcon.dlj.tf(subconverter作者提供-稳定)":
             "https://subcon.dlj.tf/sub?",
           "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
-          "api.wcc.best(sub-web作者提供-稳定)": "https://api.wcc.best/sub?"
+          "api.wcc.best(sub-web作者提供-稳定)": "https://api.wcc.best/sub?",
         },
         backendOptions: [
           { value: "http://localhost:25500/sub?" },
-          { value: "https://sub-beta.now.sh/sub?" },
+          { value: "https://subconverter-mocha.vercel.app/sub?" },
           { value: "https://subcon.dlj.tf/sub?" },
           { value: "https://api.dler.io/sub?" },
-          { value: "https://api.wcc.best/sub?" }
+          { value: "https://api.wcc.best/sub?" },
         ],
-        remoteConfig: [
-          {
-            label: "默认",
-            options: [
-              {
-                label: "不选，由接口提供方提供",
-                value: ""
-              }
-            ]
-          },
           {
             label: "ACL4SSR",
             options: [
