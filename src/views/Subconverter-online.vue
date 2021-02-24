0949e8295708b1c861d02813b1b15f4376f18335
Allen <73211772+hyt-allen-xu@users.noreply.github.com>
2021-02-24 13:47:51 +0800

    Update Subconverter-online.vue

--- src/views/Subconverter-online.vue
+++ src/views/Subconverter-online.vue
@@ -265,7 +265,7 @@ export default {
         },
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
-          "sub-beta.now.sh (自动编译最新版本后端-测试）": "https://sub-beta.now.sh/sub?",
+          "subconverter-mocha.vercel.app (自动编译最新版本后端-测试）": "https://subconverter-mocha.vercel.app/sub?",
           "subcon.dlj.tf(subconverter作者提供-稳定)":
             "https://subcon.dlj.tf/sub?",
           "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
