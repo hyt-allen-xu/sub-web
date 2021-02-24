7e09d4137ca76b940a70c63a1d0a8dbe3654a2c3
Allen <73211772+hyt-allen-xu@users.noreply.github.com>
2021-02-24 22:59:52 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -268,7 +268,7 @@ export default {
         },
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
-          "ssub.proxypoolv2.tk (Allen Xu 提供-稳定）": "https://sub.proxypoolv2.tk/sub?",
+          "sub.proxypoolv2.tk (Allen Xu 提供-稳定）": "https://sub.proxypoolv2.tk/sub?",
           "subcon.dlj.tf(subconverter作者提供-稳定)":
             "https://subcon.dlj.tf/sub?",
           "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
